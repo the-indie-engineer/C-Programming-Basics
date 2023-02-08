693911bab273611c18274f64aef1542df3ddbe3a
Thiruvarulselvan Karunanithi <109530150+the-indie-engineer@users.noreply.github.com>
2023-02-08 20:35:25 +0530

    Update find_no_handshakes.c

--- Problems/Easy/find_no_handshakes.c
+++ Problems/Easy/find_no_handshakes.c
@@ -1,6 +1,23 @@
 /*
 find_no_handshakes.c
 
+At the annual meeting of Board of Directors of Acme Inc, every one starts shaking hands with everyone else in the room. Given the fact that any two persons shake hand exactly once, Can you tell the total count of handshakes?
+
+Input format
+Input contains an integer N, the total number of Board of Directors of Acme.
+
+Output format
+Print the number of handshakes.
+
+Code constraints
+0 <=N <=10000
+
+Sample testcases
+Input 1
+1
+Output 1
+0
+
 Author: Thiruvarulselvan K, https://github.com/the-indie-engineer
 Date created: 08 Feb 2023
 */
@@ -14,5 +31,5 @@ int handshake(int n) {
         num+=n;
         n--;
     }
-    return num;
+    return num; //returns the number of handshakes
 }
